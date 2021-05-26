880361c2b93dd123526b0ba412028e17790325fa
mazerty <mazerty@gmail.com>
2021-05-26 17:06:38 +0200

    utilisation du scanner

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,12 +4,15 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public void execute() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
         Socket sock = new Socket("127.0.0.1", 1235);
         try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()))) {
-            bw.write("hey !");
+            bw.write(scanner.nextLine());
             bw.newLine();
             bw.flush();
         }
